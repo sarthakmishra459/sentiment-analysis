9cd9f7d5965ad6dba8b3c88b50945efb4b2f99d5
Sarthak Mishra <124512392+sarthakmishra459@users.noreply.github.com>
2024-05-08 11:15:24 +0530

    Sentiment-Analysis

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,91 @@
-import Image from "next/image";
+"use client"
+import { useState } from 'react';
 
 export default function Home() {
+  // State variables for tweet input and predicted sentiment
+  const [tweet, setTweet] = useState('');
+  const [sentiment, setSentiment] = useState('');
+
+  // Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Make a POST request to the FastAPI endpoint
+    const response = await fetch('https://backend-zgdl.onrender.com/predict_sentiment', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ tweet }),
+    });
+
+    // Parse the JSON response
+    const data = await response.json();
+
+    // Update the state with the predicted sentiment
+    setSentiment(data.sentiment);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
+    /*<div className="max-w-md mx-auto p-4 bg-white rounded-lg shadow-md">
+      <h1 className="text-2xl font-semibold mb-4">Sentiment Analysis</h1>
+      <form onSubmit={handleSubmit}>
+        <label className="block mb-4">
+          <span className="text-gray-700">Enter your tweet:</span>
+          <input
+            type="text"
+            value={tweet}
+            onChange={(e) => setTweet(e.target.value)}
+            className="mt-1 block w-full rounded-md text-black border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
+        </label>
+        <button
+          type="submit"
+          className="inline-block bg-indigo-500 text-black py-2 px-4 rounded-lg hover:bg-indigo-600"
+        >
+          Predict Sentiment
+        </button>
+      </form>
+      {sentiment && (
+        <p className="mt-4 text-black">
+          The predicted sentiment for your tweet "<span className="font-semibold">{tweet}</span>" is: <span className={sentiment === 'Positive' ? 'text-green-500 font-semibold' : sentiment==='Neutral'?'text-blue-500 font-semibold' : 'text-red-700 font-semibold'}>{sentiment}</span>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+      )}
+    </div>*/
+    <div className="flex justify-center items-center h-screen">
+      <div className="h-[20vw] w-full max-w-md bg-gray-800 border border-white rounded-lg p-8">
+        <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+          <label className="flex flex-col">
+            <span className="text-white leading-none tracking-tight font-[Neue_Montreal] mb-1">Enter tweet:</span>
+            <input
+              type="text"
+              value={tweet}
+              onChange={(e) => {
+                setTweet(e.target.value)
+                setSentiment('')
+              }}
+              className="text-black rounded-md cursor-text border-collapse bg-gray-200 px-4 py-2"
             />
-          </a>
-        </div>
+          </label>
+          <button
+            type="submit"
+            className="text-white bg-orange-600 rounded-md text-md py-2 focus:outline-none hover:bg-orange-700 transition-colors duration-300 ease-in-out"
+          >
+            Predict Sentiment
+          </button>
+        </form>
+        {sentiment && (
+          <p className="text-white mt-4">
+            The predicted sentiment for your tweet "<span className="font-semibold">{tweet}</span>" is:{" "}
+            <span className={sentiment === "Positive" ? "text-green-500 font-semibold" : sentiment === "Neutral" ? "text-yellow-500 font-semibold" : "text-red-700 font-semibold"}>
+              {sentiment}
+            </span>
+          </p>
+        )}
       </div>
+    </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
+
 }
